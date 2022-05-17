abc68da49457ca6c8064e3b255a82abe65beb560
jupycodes <c.sabo@iive.ca>
2022-05-16 22:03:43 -0600

    changed config.js to production env variables

--- config.js
+++ config.js
@@ -1,4 +1,17 @@
+const environment = process.env.NODE_ENV || "production"
+
+if(environment === "production") {
+    host = 'exbodcemtop76rnz.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
+    database = 'bhbbkvlxga29ps0e';
+    username = 'hml0fknte0is3c57';
+    password= 'fjkvfja2bf4vrkrw';
+} 
+
 const Sequelize = require('sequelize');
-const config = new Sequelize('shopify_inventory','cristina1', 'cristinAA$$33', {dialect: 'mysql'});
+const config = new Sequelize(
+    database,
+    username,
+    password, 
+    {dialect: 'mysql'});
 
 module.exports = config;
